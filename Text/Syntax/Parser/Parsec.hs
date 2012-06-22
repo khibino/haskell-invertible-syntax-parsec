046be90c53cd1f739cc1c783ae7c1185bdeaa402
HIBINO Kei <hibi@utmc.or.jp>
2012-06-22 23:33:02 +0900

    Initial commit. Syntax instance definition for Parsec.

---
+++ Text/Syntax/Parser/Parsec.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+
+module Text.Syntax.Parser.Parsec (
+  -- * Run Syntax as Parsec
+  runPolyParser
+  ) where
+
+import Text.Syntax.Poly.Instances ()
+import Text.Syntax.Poly
+  ((<||>), TryAlternative(try, (<|>)),
+   StreamSyntax (..), Syntax(..), RunParserT)
+
+import Text.Parsec (ParsecT, Stream, ParseError)
+import qualified Text.Parsec as P
+
+-- ParsecT s u m is instance of MonadPlus.
+-- Instance definitions of IsoFunctor, ProductFunctor, IsoAlternative and AbstractSyntax
+-- is derived from MonadPlus by definitions in Text.Syntax.Poly.Instances
+
+instance TryAlternative (ParsecT s u m)
+  where try = P.try
+        p <|> q = try p <||> q
+
+instance (Eq t, Show t, Stream [t] m t) =>
+         StreamSyntax [t] (ParsecT [t] u m)
+  where string tks = P.tokens show (\pos _ -> pos) tks >> return ()
+
+instance (StreamSyntax [t] (ParsecT [t] u m), Stream [t] m t, Show t) =>
+         Syntax t [t] (ParsecT [t] u m)
+  where token = P.anyToken
+
+runPolyParser :: (Eq tok, Show tok) =>
+                 String -> RunParserT tok [tok] a ParseError
+runPolyParser srcName parser =
+  fmap (flip (,) []) . P.parse parser srcName
---
+++ Text/Syntax/Parser/Parsec/Check.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE Rank2Types #-}
+
+module Text.Syntax.Parser.Parsec.Check (
+  -- * Handy check isomorphism functions.
+  printParseIsoDefault',  printParseIsoDefault
+  ) where
+
+import Text.Syntax.Poly (SyntaxT)
+
+import Text.Syntax.Printer.List (runPolyPrinter)
+import Text.Syntax.Poly.Check (printParseIso', printParseIso)
+
+import Text.Syntax.Parser.Parsec (runPolyParser)
+
+printParseIsoDefault' :: (Eq tok, Eq a, Show tok) => SyntaxT tok [tok] a -> a -> Either String a
+printParseIsoDefault' =  printParseIso' runPolyPrinter (runPolyParser "<check>")
+
+printParseIsoDefault :: (Eq tok, Eq a, Show tok) => SyntaxT tok [tok] a -> [tok] -> Either String a
+printParseIsoDefault =  printParseIso runPolyPrinter (runPolyParser "<check>")
