bc271ee3ed139965564be8182d1b39da96c83c15
Kei Hibino <hibi@utmc.or.jp>
2012-08-15 08:37:13 +0900

    Update along with invertible-syntax-poly

--- Text/Syntax/Check/Parsec.hs
+++ Text/Syntax/Check/Parsec.hs
@@ -3,18 +3,18 @@
 
 module Text.Syntax.Check.Parsec (
   -- * Handy isomorphism check functions.
-  printParseIsoDefault',  printParseIsoDefault
+  printParseIso0Default,  printParseIsoDefault
   ) where
 
 import Text.Syntax.Poly (SyntaxT)
 
 import Text.Syntax.Printer.List (runAsPrinter)
-import Text.Syntax.Check.Prim (printParseIso', printParseIso)
+import Text.Syntax.Check.Prim (printParseIso0, printParseIso)
 
 import Text.Syntax.Parser.Parsec (runAsParsec)
 
-printParseIsoDefault' :: (Eq tok, Eq a, Show tok) => SyntaxT tok a -> a -> Either String a
-printParseIsoDefault' =  printParseIso' runAsPrinter (runAsParsec "<check>")
+printParseIso0Default :: (Eq tok, Eq a, Show tok) => SyntaxT tok a -> a -> Either String a
+printParseIso0Default =  printParseIso0 runAsPrinter (runAsParsec "<check>")
 
 printParseIsoDefault :: (Eq tok, Eq a, Show tok) => SyntaxT tok a -> [tok] -> Either String a
 printParseIsoDefault =  printParseIso runAsPrinter (runAsParsec "<check>")
